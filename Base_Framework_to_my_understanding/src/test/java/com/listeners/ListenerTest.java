2d97ce1973d82a15ce06ddb1877d572c4c46778d
Ashwini.G <Ashwini.G@SSTINADMLP1150.sstech.internal>
2022-10-28 13:14:17 +0530

    first push

---
+++ Base_Framework_to_my_understanding/src/test/java/com/listeners/ListenerTest.java
@@ -0,0 +1,52 @@
+package com.listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+// as we mentioned listeners in testng.xml we have to create a listeners class. then we can listen to test cases. we used it to capture screen shots.
+
+public class ListenerTest implements ITestListener {
+
+	@Override
+	public void onFinish(ITestContext result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStart(ITestContext result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+}
